780fcd9c8f3673a6e7fe3addee067e5d0b34608b
Filip <filip.trifunovic23@gmail.com>
2022-10-20 12:31:10 +0200

    create components, set up context

---
+++ src/context.js
@@ -0,0 +1,76 @@
+import axios from "axios";
+import React, { useState, useContext, useEffect } from "react";
+
+const table = {
+  sports: 21,
+  history: 23,
+  politics: 24,
+};
+
+const API_ENDPOINT = "https://opentdb.com/api.php?";
+
+const url = "";
+
+const tempUrl =
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [waiting, setWaiting] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [error, setError] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [quiz, setQuiz] = useState({
+    amount: 10,
+    category: "sports",
+    difficulty: "easy",
+  });
+
+  async function getQuestion(url) {
+    setLoading(true);
+    setWaiting(false);
+
+    const response = await axios(url).catch((err) => alert(err));
+
+    if (response) {
+      const data = response.data.results;
+
+      if (data.length > 1) {
+        setQuestions(data);
+        setLoading(false);
+        setWaiting(false);
+        setError(false);
+      } else {
+        setWaiting(true);
+        setError(true);
+      }
+    } else {
+      setWaiting(true);
+    }
+  }
+
+  return (
+    <AppContext.Provider value="hello">
+      {{
+        waiting,
+        loading,
+        questions,
+        index,
+        correct,
+        error,
+        isModalOpen,
+        quiz,
+      }}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
